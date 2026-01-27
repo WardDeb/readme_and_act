a4fef84c521f654e992d79fbefa2ca17e28465fe
Ward D <w@rddeboutte.com>
2026-01-27 16:33:56 +0100

    feat: toml option for gh configs (#9)
    refactor: gh defaults in separate python file

--- src/raa/main.py
+++ src/raa/main.py
@@ -10,6 +10,7 @@ def main():
     parser.add_argument('--max_lines', type=int, default=5, help='Maximum number of activity lines to show')
     parser.add_argument('--repo', default=None, required=False, help='Repository name to commit changes to. Defaults to github.com/username/username')
     parser.add_argument('--test', action='store_true', help='Run in test mode without committing changes')
+    parser.add_argument('--cfg', type=str, default=None, help='Path to configuration file (toml)')
     args = parser.parse_args()
 
     if not args.username:
@@ -22,6 +23,9 @@ def main():
         args.repo = os.getenv('REPO_NAME')
     if not args.repo:
         args.repo = f"{args.username}/{args.username}"
+    if not args.cfg:
+        if os.getenv('CFG_PATH'):
+            args.cfg = os.getenv('CFG_PATH')
     
     github_token = os.getenv('GH_TOKEN')
     
@@ -31,7 +35,8 @@ def main():
         test = args.test,
         num_events = args.max_lines,
         gh_repo = args.repo,
-        github_token = github_token
+        github_token = github_token,
+        cfg = args.cfg
     )
     p.fetch_activity()
     p.construct_readme_section()
